f9f66f9cbcf60a8e157e69f140fe82aaa821e33c
Mart Walt <mawkeye@gmail.com>
2025-11-17 23:03:30 +0100

    CH3L5 - Sprite Groups

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = updatable, drawable
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -21,10 +27,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        
+        for item in drawable:
+            item.draw(screen)
+        
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000        
